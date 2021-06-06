a50b1ab0ddb4675d8afd234eef8f79f9303096ca
Jason Butz <736696+jbutz@users.noreply.github.com>
2021-06-06 19:07:37 -0400

    Add JavaScript exercise

---
+++ javascript/README.md
@@ -0,0 +1,6 @@
+# JavaScript Exercise
+
+- Open `index.html` in a modern web browser
+- Open `script.js` in your code editor
+
+The `index.html` file has the exercise instructions.
---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+/*
+ * You must have a function called `run` in your solution.
+ * The `run` function must return a value in order for it to be displayed on the page.
+ * The `run` function must keep the `async` keyword or return a Promise.
+ */
+async function run() {
+    return 'sample';
+}
+
+async function getExchangeRate(from, to) {
+    const resp = await fetch(`https://2021-new-hire.vercel.app/api/exchange/${from}/${to}`);
+    return await resp.json();
+}
